36a1b2a2235e676776a527d490a60f5fc0c279eb
vhall1 <victorhallpsn@hotmail.com>
2025-08-29 13:05:30 +0100

    massively simplify dao

--- service.identity/dao/user.go
+++ service.identity/dao/user.go
@@ -1,17 +1,42 @@
 package dao
 
-import "time"
+import (
+	"database/sql"
+	"time"
+)
 
 type User struct {
-	ID        string
+	ID        uint32
 	Name      string
 	CreatedAt time.Time
 	UpdatedAt time.Time
 }
+type UserDAO struct {
+	db *sql.DB
+}
 
-type UserDAO interface {
-	CreateUser(user *User) error
-	GetUserByID(id string) (*User, error)
-	UpdateUser(user *User) error
-	DeleteUser(id string) error
+func NewUserDAO(db *sql.DB) *UserDAO {
+	return &UserDAO{db: db}
+}
+
+func (d *UserDAO) CreateUser(user *User) error {
+	query := `INSERT INTO "User" ("name", "updatedAt") VALUES ($1, $2) RETURNING "id", "createdAt", "updatedAt"`
+	updatedAt := time.Now()
+	return d.db.QueryRow(query, user.Name, updatedAt).Scan(&user.ID, &user.CreatedAt, &user.UpdatedAt)
+}
+
+func (d *UserDAO) GetUserByID(id string) (*User, error) {
+	query := `SELECT "id", "name", "createdAt", "updatedAt" FROM "User" WHERE "id" = $1`
+	user := &User{}
+	err := d.db.QueryRow(query, id).Scan(&user.ID, &user.Name, &user.CreatedAt, &user.UpdatedAt)
+	if err != nil {
+		return nil, err
+	}
+	return user, nil
+}
+
+func (d *UserDAO) UpdateUser(user *User) error {
+	query := `UPDATE "User" SET "name" = $1, "updatedAt" = $2 WHERE "id" = $3 RETURNING "name", "updatedAt"`
+	updatedAt := time.Now()
+	return d.db.QueryRow(query, user.Name, updatedAt, user.ID).Scan(&user.Name, &user.UpdatedAt)
 }
