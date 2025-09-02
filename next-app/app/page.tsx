32e0de1be3dcf4095d7c9bf3de4add741dd945ea
vhall1 <victorhallpsn@hotmail.com>
2025-09-02 14:18:21 +0100

    show authed userId on root page

--- next-app/app/page.tsx
+++ next-app/app/page.tsx
@@ -1,3 +1,7 @@
-export default function Home() {
-  return <div>Hello</div>;
+import { getUserId } from "@/lib/auth";
+
+export default async function Home() {
+  const userId = await getUserId();
+
+  return <div>Hello {userId}</div>;
 }
