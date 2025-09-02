aa643f10c9ab153a206c15adf7049b01e0ecc47e
vhall1 <victorhallpsn@hotmail.com>
2025-09-02 14:15:46 +0100

    remove unused server action

--- next-app/app/(auth)/login/page.tsx
+++ next-app/app/(auth)/login/page.tsx
@@ -1,33 +1,11 @@
-import { prisma } from "@/lib/prisma";
-
 export default function Login() {
-  async function handleSubmit(formData: FormData) {
-    "use server";
-
-    const username = formData.get("user");
-
-    // validate
-    if (typeof username !== "string") {
-      throw new Error("Invalid input");
-    }
-
-    // authenticate
-    const user = await prisma.user.findFirst({
-      where: { name: username },
-    });
-
-    if (!user) {
-      throw new Error("User not found");
-    }
-  }
-
   return (
     <div>
       <h1>Login</h1>
-      <form action={handleSubmit}>
+      <form action="/api/auth/login" method="post">
         <label>
           user:
-          <input type="text" name="user" />
+          <input type="text" name="username" />
         </label>
         <button type="submit">Login</button>
       </form>
