17aaf8ca140d6a6a33597338cc6db972b3a7932c
vhall1 <victorhallpsn@hotmail.com>
2025-09-08 21:45:58 +0100

    init user store

--- service.identity/handler/router.go
+++ service.identity/handler/router.go
@@ -3,6 +3,8 @@ package handler
 import (
 	"database/sql"
 	"net/http"
+
+	"github.com/vhall1/foodlog/service.identity/store"
 )
 
 type Router struct {
@@ -10,6 +12,7 @@ type Router struct {
 }
 
 func SetupRoutes(h *http.ServeMux, r *Router) {
-	h.Handle("POST /login", r.PostLogin())
-	h.Handle("POST /register", r.PostRegister())
+	userStore := store.NewUserStore(r.Database)
+	h.Handle("POST /login", r.PostLogin(userStore))
+	h.Handle("POST /register", r.PostRegister(userStore))
 }
